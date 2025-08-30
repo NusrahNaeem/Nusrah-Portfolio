5965b5c73a90657cbb978b80ca1810ddc6782038
Nusrah Naeem <nusrah.naeem111@gmail.com>
2025-08-30 16:29:05 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite"
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+
+
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
