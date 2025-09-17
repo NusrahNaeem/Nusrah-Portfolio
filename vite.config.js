931eac18cf13ddaa88c7b26f6cfa60c3cdf6c3d1
Nusrah Naeem <nusrah.naeem111@gmail.com>
2025-09-17 22:11:09 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,41 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from "@tailwindcss/vite"
+// import path from 'path'
+// import { fileURLToPath } from 'url'
+
+
+
+
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//     base: "/PORTFOLIO-Nusrah-Naeem/",
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
 import path from 'path'
 import { fileURLToPath } from 'url'
-
-
-
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: "/PORTFOLIO-Nusrah-Naeem/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
-
-
+})
 
